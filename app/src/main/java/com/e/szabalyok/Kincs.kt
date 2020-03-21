210e6fdcea17c6579d7711df2b8e82946bd946fb
janoserdody <37834280+janoserdody@users.noreply.github.com>
2020-03-22 00:48:34 +0100

    small fixes

--- app/src/main/java/com/e/szabalyok/Kincs.kt
+++ app/src/main/java/com/e/szabalyok/Kincs.kt
@@ -2,18 +2,18 @@ package com.e.szabalyok
 
 import com.e.jatekter.JatekElem
 import com.e.jatekter.JatekTer
-import com.e.jatekter.Jatekos
+//import com.e.jatekter.Jatekos
 import com.e.jatekter.RogzitettJatekElem
 
-class Kincs(x: Int, y: Int, jatekTer: JatekTer?) :
+class Kincs(x: Int, y: Int, jatekTer: JatekTer) :
     RogzitettJatekElem(x, y, jatekTer) {
     override val meret: Double
         get() = 1.0
 
-    override fun utkozes(jatekElem: JatekElem?) {
-        if (jatekElem is Jatekos) {
-            jatekElem.pontotSzerez(50)
-        }
-        ter.Torles(this)
+   override fun utkozes(jatekElem: JatekElem) {
+        //if (jatekElem is Jatekos) {
+        //    jatekElem.pontotSzerez(50)
+        //}
+       // ter.Torles(this)
     }
 }

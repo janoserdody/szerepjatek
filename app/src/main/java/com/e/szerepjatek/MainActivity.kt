db54bed21943f6289cba35c37a62d77a548f71d9
janoserdody <37834280+janoserdody@users.noreply.github.com>
2020-03-21 16:36:07 +0100

    spike monsters

--- app/src/main/java/com/e/szerepjatek/MainActivity.kt
+++ app/src/main/java/com/e/szerepjatek/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.e.szerepjatek
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button1)
+        button?.setOnClickListener()
+        {
+            val imageMonsterView = findViewById<ImageView>(R.id.imageView1)
+            imageMonsterView.setImageResource(R.drawable.monster2);
+            imageMonsterView.invalidate()
+        }
+
     }
 }
